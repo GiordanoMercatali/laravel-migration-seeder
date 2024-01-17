4327230223c155cb4e4f59e863767750670fb4fe
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-17 10:36:06 +0100

    minor corrections

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Movie;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+    <h1>Trains</h1>
+    <ul>
+        @foreach ($trains as $train)
+            <li>
+                <span>The train from {{$train->start_station}} heading to {{$train->arrival_station}} is now arriving </span>
+                @if ($train->is_on_time)
+                    <span>on time</span>
+                @else
+                    <span>with a delay</span>
+                @endif
+                <span> at the platform.</span>
+            </li>
+        @endforeach
+    </ul>
 @endsection
